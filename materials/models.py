30c5b91f9364df35a9dbfc57ec2512b13ac04dab
crystal <139240411+DaryaAgafonova@users.noreply.github.com>
2025-05-02 05:40:49 +0300

    Add files via upload

---
+++ materials/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField('Название', max_length=255)
+    preview = models.ImageField('Превью', upload_to='courses/previews/')
+    description = models.TextField('Описание')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+    title = models.CharField('Название', max_length=255)
+    description = models.TextField('Описание')
+    preview = models.ImageField('Превью', upload_to='lessons/previews/')
+    video_url = models.URLField('Ссылка на видео')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['course', 'id']
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
